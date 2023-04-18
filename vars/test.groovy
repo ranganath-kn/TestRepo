2a7c3aead50976208b4f5ef6db7010f1ab04ff59
Ranganath K N <62882460+ranganath-kn@users.noreply.github.com>
2023-04-18 16:26:25 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,1 +1,14 @@
 
+def call(){
+	def appName = "Test"
+	def deploy = "T1"
+	def techno = "Another Test"
+	echo "New Logic"
+	sh """
+	cat >> ./${appName}-prop.file <<EOF
+	name=${appName}
+	operation=${deploy}
+	tech=${techno}
+	trigger=${BUILD_USER}
+	"""
+}
